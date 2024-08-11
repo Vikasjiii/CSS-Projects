a9ddbb1235057f49f18fc1d6133c9841b5f94e16
Vikas Sahni <vikassahni8361@gmail.com>
2024-08-12 03:17:51 +0530

    finally completed

--- 01_JS_Documentation/script.js
+++ 01_JS_Documentation/script.js
@@ -1,7 +1,15 @@
 const copyButton = document.querySelector(".code-container img");
 const copy = document.querySelector(".code-container");
+const copyMessage=document.querySelector('.copied-msg')
 copyButton.addEventListener("click", () => {
   const copyText = copy.innerText;
 
   navigator.clipboard.writeText(copyText);
+  addClassToCopyButton()
 });
+function addClassToCopyButton(){
+  copyMessage.classList.add('show')
+  setTimeout(()=>{
+    copyMessage.classList.remove('show')
+  },500)
+}
